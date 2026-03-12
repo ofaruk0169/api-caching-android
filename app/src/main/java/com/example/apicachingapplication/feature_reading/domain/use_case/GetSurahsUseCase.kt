6a7e8b7a8a3d51b8e7b97d2debed2ed75e479f5f
Omare Faruk <ofaruk0169@gmail.com>
2026-03-12 17:02:33 +0000

    working on it

--- app/src/main/java/com/example/apicachingapplication/feature_reading/domain/use_case/GetSurahsUseCase.kt
+++ app/src/main/java/com/example/apicachingapplication/feature_reading/domain/use_case/GetSurahsUseCase.kt
@@ -1,4 +1,28 @@
 package com.example.apicachingapplication.feature_reading.domain.use_case
 
-class GetSurahsUseCase {
+import com.example.apicachingapplication.core.Resource
+import com.example.apicachingapplication.feature_reading.data.remote.dto.toSurah
+import com.example.apicachingapplication.feature_reading.domain.repository.SurahRepository
+import kotlinx.coroutines.flow.Flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetSurahsUseCase @Inject constructor(
+    private val repository: SurahRepository
+) {
+
+
+    operator fun invoke(): Flow<Resource<List>Surah>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val surahs = repository.getSurahs().map { it.toSurah() }
+            emit(Resource.Success(surahs))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected  error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Please check connection."))
+        }
+    }
+
 }
