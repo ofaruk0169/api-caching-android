0ee2f1afdf0edb2f568057d4cd2ff96f7efde286
Omare Faruk <omarefaruk@outlook.com>
2026-08-28 23:56:55 +0100

    brainstorm

--- app/src/main/java/com/example/apicachingapplication/feature_reading/presentation/quran_surah_list/SurahListScreen.kt
+++ app/src/main/java/com/example/apicachingapplication/feature_reading/presentation/quran_surah_list/SurahListScreen.kt
@@ -1,6 +1,7 @@
 package com.example.apicachingapplication.feature_reading.presentation.quran_surah_list
 
 import android.R.attr.top
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,8 +9,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -19,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -47,17 +51,21 @@ fun SurahListScreen(
                     }
                 )
             }
-
-            item {
-                IconButton(onClick = { /* handle download */ }) {
-                    Icon(
-                        imageVector = Icons.Filled.KeyboardArrowDown,
-                        contentDescription = "Download"
-                    )
-                }
-            }
-
         }
+
+      /*  IconButton(
+            onClick = { *//* handle download *//* },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+                .background(color = Color.Magenta, shape = CircleShape)
+                .size(56.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Download"
+            )
+        }*/
 
         if(state.error.isNotBlank()) {
             Text(
