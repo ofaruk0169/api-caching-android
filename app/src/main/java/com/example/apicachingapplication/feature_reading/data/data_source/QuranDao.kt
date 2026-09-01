da21971077d34ed8e6aa5b49f3fbc4c05f4641c8
Omare Faruk <omarefaruk@outlook.com>
2026-09-01 17:24:23 +0100

    improved qurandao

--- app/src/main/java/com/example/apicachingapplication/feature_reading/data/data_source/QuranDao.kt
+++ app/src/main/java/com/example/apicachingapplication/feature_reading/data/data_source/QuranDao.kt
@@ -2,13 +2,16 @@ package com.example.apicachingapplication.feature_reading.data.data_source
 
 import androidx.room.Dao
 import androidx.room.Upsert
+import com.example.apicachingapplication.feature_reading.domain.model.AyahEntity
 import com.example.apicachingapplication.feature_reading.domain.model.SurahDetail
 import com.example.apicachingapplication.feature_reading.domain.model.SurahEntity
 
 @Dao
 interface QuranDao {
+    @Upsert
+    suspend fun upsertSurah(surah: SurahEntity)
 
     @Upsert
-    suspend fun upsertTask(task: SurahEntity)
+    suspend fun upsertAyahs(ayahs: List<AyahEntity>)
 
 }
--- app/src/main/java/com/example/apicachingapplication/feature_reading/domain/use_case/CacheSurahUseCase.kt
+++ app/src/main/java/com/example/apicachingapplication/feature_reading/domain/use_case/CacheSurahUseCase.kt
@@ -3,6 +3,7 @@ package com.example.apicachingapplication.feature_reading.domain.use_case
 import com.example.apicachingapplication.core.Resource
 import com.example.apicachingapplication.feature_reading.data.remote.dto.toAyahEntities
 import com.example.apicachingapplication.feature_reading.data.remote.dto.toSurahDetail
+import com.example.apicachingapplication.feature_reading.data.remote.dto.toSurahEntity
 import com.example.apicachingapplication.feature_reading.domain.model.AyahEntity
 import com.example.apicachingapplication.feature_reading.domain.model.SurahDetail
 import com.example.apicachingapplication.feature_reading.domain.repository.AyahRepository
@@ -19,8 +20,14 @@ class CacheSurahUseCase (
     operator fun invoke(surahId: String): Flow<Resource<List<AyahEntity>>> = flow {
         try {
             emit(Resource.Loading<List<AyahEntity>>())
-            val surah = repository.getSurahById(surahId).toAyahEntities()
-            emit(Resource.Success<List<AyahEntity>>(surah))
+            val surahAyah = repository.getSurahById(surahId).toAyahEntities()
+            val surah = repository.getSurahById(surahId).toSurahEntity()
+
+            val dto = repository.getSurahById(surahId)
+            val sur
+
+
+            emit(Resource.Success<List<AyahEntity>>(surahAyah))
         } catch (e: HttpException) {
             emit(Resource.Error<List<AyahEntity>>(e.localizedMessage ?: "An unexpected  error occured"))
         } catch (e: IOException) {
